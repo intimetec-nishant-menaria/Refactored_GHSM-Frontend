f34786acda9b8e53ed4411c83e7f0e1fc892155f
Khushi vijay <khushi.vijay@intimetec.com>
2026-02-24 15:53:13 +0530

    Remove extra unnecessary spaces and lines

--- src/pages/resetPassword/ResetPassword.tsx
+++ src/pages/resetPassword/ResetPassword.tsx
@@ -4,20 +4,16 @@ import { useQueryParams } from "@/hooks/useQueryParams";
 
 const ResetPassword = () => {
   const { token } = useQueryParams();
-
   if (!token) {
     return <p className="text-red-500">Invalid or missing reset token.</p>;
   }
 
   return (
-    <AuthLayout
-      title="Reset Password"
-    >
+    <AuthLayout title="Reset Password">
       <div className="text-center">
         <p className="mt-2 text-sm text-gray-500">
           Enter your new password below to reset your account.
         </p>
-
         <ResetPasswordForm />
       </div>
     </AuthLayout>
