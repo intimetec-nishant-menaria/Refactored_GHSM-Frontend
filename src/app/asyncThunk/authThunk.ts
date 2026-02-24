9a97e59681a789c196ae7689f679911923e8cf40
Khushi vijay <khushi.vijay@intimetec.com>
2026-02-24 16:24:45 +0530

    Add reusable apiThunk helper function and refactor login, forgotPassword, resetPassword thunks

--- src/app/asyncThunk/authThunk.ts
+++ src/app/asyncThunk/authThunk.ts
@@ -1,36 +1,28 @@
 import type { LoginInput } from "@/utils/schemas/loginSchema";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { api } from "@/services/api";
 import type { ForgotPasswordInput } from "@/utils/schemas/forgotPasswordSchema";
-import type { ResetPasswordPayload } from "@/utils/interfaces/ResetPasswordPayload"
+import type { ResetPasswordPayload } from "@/utils/interfaces/ResetPasswordPayload";
+import apiThunk from "@/app/asyncThunk/apiThunkHelper";
 
 export const loginUser = createAsyncThunk(
   "auth/loginUser",
   async (data: LoginInput, { rejectWithValue }) => {
     try {
-      const response = await api.post("/auth/login", data, {
-        withCredentials: true,
-      });
-      return response.data;
+      return await apiThunk("/auth/login", data, { withCredentials: true });
     } catch (error) {
-      if (error instanceof Error) {
-        return rejectWithValue(error.message);
-      }
+      if (error instanceof Error) return rejectWithValue(error.message);
       return rejectWithValue("Login failed");
     }
-  },
+  }
 );
 
 export const forgotPassword = createAsyncThunk(
   "auth/forgotPassword",
   async (data: ForgotPasswordInput, { rejectWithValue }) => {
     try {
-      const response = await api.post("/auth/forgetPassword", data);
-      return response.data;
+      return await apiThunk("/auth/forgetPassword", data);
     } catch (error) {
-      if (error instanceof Error) {
-        return rejectWithValue(error.message);
-      }
+      if (error instanceof Error) return rejectWithValue(error.message);
       return rejectWithValue("Failed to send reset link");
     }
   }
@@ -40,18 +32,14 @@ export const resetPassword = createAsyncThunk(
   "auth/resetPassword",
   async (data: ResetPasswordPayload, { rejectWithValue }) => {
     try {
-      const response = await api.post("/auth/resetPassword", {
-        email : data.email,
+      return await apiThunk("/auth/resetPassword", {
+        email: data.email,
         token: data.token,
         newPassword: data.password,
-        confirmPassword : data.confirmPassword
+        confirmPassword: data.confirmPassword,
       });
-
-      return response.data;
     } catch (error) {
-      if (error instanceof Error) {
-        return rejectWithValue(error.message);
-      }
+      if (error instanceof Error) return rejectWithValue(error.message);
       return rejectWithValue("Reset failed");
     }
   }
