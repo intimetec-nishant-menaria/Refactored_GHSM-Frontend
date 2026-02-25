cf82f33c71f927ffea1532ac81ccc90385739067
Khushi vijay <khushi.vijay@intimetec.com>
2026-02-25 12:42:22 +0530

    Changes done

--- src/app/asyncThunk/authThunk.ts
+++ src/app/asyncThunk/authThunk.ts
@@ -1,7 +1,7 @@
 import type { LoginInput } from "@/utils/schemas/loginSchema";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { ForgotPasswordInput } from "@/utils/schemas/forgotPasswordSchema";
-import type { ResetPasswordPayload } from "@/utils/interfaces/ResetPasswordPayload";
+import type { ResetPasswordPayload } from "@/utils/interfaces/resetPassword";
 import apiThunk from "./apiThunkHelper";
 import type { User } from "@/utils/interfaces/user";
 
