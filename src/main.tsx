c58072d2be3e412b46441abae2ffea714a2dae2b
Khushi vijay <khushi.vijay@intimetec.com>
2026-02-26 16:31:24 +0530

    Added all the components, related thunk, store, slice and pages that is related to user and room management

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./App.tsx";
+import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "@/app/store/store.ts";
 import { BrowserRouter } from "react-router-dom";
-import Dashboard from "./pages/dashboard/Dashboard.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        {/* <App /> */}
-        <Dashboard />
+        <App />
       </Provider>
     </BrowserRouter>
   // </StrictMode>,
