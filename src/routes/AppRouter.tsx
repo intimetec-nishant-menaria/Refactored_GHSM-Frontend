7e288c4860d086a976b2a5950bac4c93372be172
Khushi vijay <khushi.vijay@intimetec.com>
2026-02-26 16:27:12 +0530

    Set the routes in the AppRouter for the /admin/dashboard and admin/userManagement

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,8 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Login from "@/pages/login/Login";
 import ForgotPassword from "@/pages/forgotPassword/ForgotPassword";
 import ResetPassword from "@/pages/resetPassword/ResetPassword";
-import Dashboard from "@/pages/dashboard/Dashboard";
 import AdminLayout from "@/components/layouts/AdminLayout";
+import DashboardCards from "@/components/common/dashboard/dashboardCards";
+import UserManagement from "@/components/userManagement/userManagement";
+import RoomManagement from "@/components/roomManagement/roomManagement";
 
 export default function AppRouter() {
   return (
@@ -11,11 +13,10 @@ export default function AppRouter() {
       <Route path="/" element={<Login />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
-
       <Route path="/admin" element={<AdminLayout />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        {/* <Route path="rooms" element={<Rooms />} />
-        <Route path="users" element={<Users />} /> */}
+        <Route path="dashboard" element={<DashboardCards />} />
+        <Route path="users" element={<UserManagement />} />
+        <Route path="rooms" element={<RoomManagement />} />
       </Route>
     </Routes>
   );
