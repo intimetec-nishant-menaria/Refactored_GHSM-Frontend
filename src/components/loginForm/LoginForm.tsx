eed7424a9aa43efa01b72be15201b144ed828f52
Nishant Menaria <nishant.menaria@intimetec.com>
2026-03-01 18:10:01 +0530

    secure routes based on authentication state

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -12,7 +12,6 @@ import type { AppDispatch } from "@/app/store/store";
 import { useRef } from "react";
 import { loginUser } from "@/app/asyncThunk/authThunk";
 import { useEffect } from "react";
-import { check } from "zod";
 
 const LoginForm = () => {
   const navigate = useNavigate();
